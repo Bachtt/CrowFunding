9f80d3922461bc094686577e7aee15f7813e50ee
bachtt <bachtt107@gmail.com>
2023-06-05 16:19:58 +0700

    code trang sign-up P2

--- src/layout/LayoutAuthentication.js
+++ src/layout/LayoutAuthentication.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withErrorBoundary } from "react-error-boundary";
+import ErrorComponent from "../components/common/ErrorComponent";
 
 const LayoutAuthentication = (props) => {
-    const {children, heading = ""} = props;
-    return (
-        <div className='w-full min-h-screen bg-lite p-10 relative isolate'>
-            <img src='/ellipse.png' alt='bg' className='pointer-events-none absolute bottom-0 left-0 right-0 z-[-1]'/>
-            <Link to="/" className='block mb-5 lg:mb-16'>
-                <img srcSet='/logo.png 2x' alt='crowfunding-app' />
-            </Link>
-            <div className='w-full max-w-[556px] bg-white rounded-lg px-5 py-8 lg:px-16 lg:py-12 mx-auto'>
-                <h1 className='font-semibold text-lg lg:text-xl mb-1 lg:mb-3 text-center text-text1'>{heading}</h1>
-                {children}   
-            </div>
-        </div>
-    );
+  const { children, heading = "" } = props;
+  return (
+    <div className="w-full min-h-screen bg-lite p-10 relative isolate">
+      <img
+        src="/ellipse.png"
+        alt="bg"
+        className="pointer-events-none absolute w-full bottom-0 left-0 right-0 z-[-1]"
+      />
+      <Link to="/" className="block mb-5 lg:mb-16">
+        <img srcSet="/logo.png 2x" alt="crowfunding-app" />
+      </Link>
+      <div className="w-full max-w-[556px] bg-white rounded-lg px-5 py-8 lg:px-16 lg:py-12 mx-auto">
+        <h1 className="font-semibold text-lg lg:text-xl mb-1 lg:mb-3 text-center text-text1">
+          {heading}
+        </h1>
+        {children}
+      </div>
+    </div>
+  );
 };
 
-export default LayoutAuthentication;
+LayoutAuthentication.propTypes = {
+  heading: PropTypes.string,
+  children: PropTypes.node,
+};
+export default withErrorBoundary(LayoutAuthentication, {
+  FallbackComponent: ErrorComponent,
+});
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import LayoutAuthentication from '../layout/LayoutAuthentication';
+import React from "react";
+import LayoutAuthentication from "../layout/LayoutAuthentication";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Label } from "../components/label";
+import { Input } from "../components/input";
 
 const SignUpPage = () => {
-    return (
-        <LayoutAuthentication heading="SignUp">
-            Lorem 
-        </LayoutAuthentication>
-    );
+  const { handleSubmit, control } = useForm({});
+  return (
+    <LayoutAuthentication heading="SignUp">
+      <p className="text-center  font-normal text-xs lg:text-sm text-text3 mb-6 lg:mb-8">
+        Already have an account?{" "}
+        <Link to="/sign-in" className="text-primary font-medium underline">
+          Sign In
+        </Link>
+      </p>
+      <button className="flex items-center gap-x-3 w-full justify-center py-4 border border-strock rounded-xl text-text2 text-base font-semibold mb-5">
+        <img srcSet="/icon-google.png 2x" alt="icon-google" />
+        <span>Sign Up with Google</span>
+      </button>
+      <p className="text-center font-normal text-xs lg:text-sm mb-4 lg:mb-8 text-text2">
+        Or Sign Up with email
+      </p>
+      <form>
+        <div className="flex flex-col gap-y-3">
+          <Label>Full Name *</Label>
+          <Input control={control} name="name"></Input>
+        </div>
+      </form>
+    </LayoutAuthentication>
+  );
 };
 
 export default SignUpPage;
